cf648d07671bad883f18b80e9961057e608eda7e
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-11 18:51:17 +0530

    Created card prefabs and related scripts, Player can click to assign.

---
+++ Bingo/Assets/CardRelay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CardRelay : NetworkBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
