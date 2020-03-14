097af04620ddc691b4f11cc9003ad2dae1373881
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-14 23:53:44 +0530

    Connected players' names are not displayed.

---
+++ Bingo/Assets/UpdateConnectedPlayersList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateConnectedPlayersList : MonoBehaviour
+{
+    public GameObject AllPlayersObj;
+    private TextMeshProUGUI connectedPlayersText;
+    
+    void Start(){
+        connectedPlayersText = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+    
+    void FixedUpdate()
+    {
+        string connectedListString = "";
+        int children = AllPlayersObj.transform.childCount;
+        
+        foreach (Transform child in AllPlayersObj.transform)
+        {
+        //              child.gameObject.SetActive(false);
+            ClientPlayerScript clientPlayerScript = child.gameObject.GetComponent<ClientPlayerScript>();
+            connectedListString += clientPlayerScript.playerName + "\n";
+        }
+        connectedPlayersText.text = connectedListString;
+//        for (int i = 0; i < children; i++){
+//            ClientPlayerScript clientPlayerScript = transform.GetChild(i).gameObject.GetComponent<ClientPlayerScript>();
+//            connectedList += clientPlayerScript.playerName + "\n";
+//        }
+    }
+}
