58a27c07d48d18cc39e9c36348cd1d2188e692e3
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-15 23:53:37 +0530

    Imported Photon

---
+++ Bingo/Assets/photonConnect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class photonConnect : MonoBehaviour
+{
+    public string versionname = "0.1";
+    
+    
+    public GameObject player;
+    
+    public GameObject connected, disconnected, connect;
+    public void connectToPhoton(){
+        PhotonNetwork.ConnectUsingSettings(versionname);
+        
+        Debug.Log("Connecting To photon");
+    }
+    
+    private void OnConnectedToMaster(){
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        
+        connect.SetActive(false);
+        connected.SetActive(true);
+        Debug.Log("Connected to lobby");
+    }
+    
+    private void OnJoinedLobby(){
+        Debug.Log("On joined Lobby");
+    }
+    
+    private OnDisconnectedFromPhoton(){
+    
+        connected.SetActive(true);
+        Debug.Log("Disconnected from photon services");
+    }
+}
