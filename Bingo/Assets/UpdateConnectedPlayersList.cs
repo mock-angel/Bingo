b0c760cf3d5a10b0d4a4f31015cc4f2537583b37
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-15 14:13:47 +0530

    Player list now displays current turn and ready.

--- Bingo/Assets/UpdateConnectedPlayersList.cs
+++ Bingo/Assets/UpdateConnectedPlayersList.cs
@@ -18,10 +18,23 @@ public class UpdateConnectedPlayersList : MonoBehaviour
         string connectedListString = "";
         foreach (Transform child in AllPlayersObj.transform)
         {
+            string clientDisplayName;
             ClientPlayerScript clientPlayerScript = child.gameObject.GetComponent<ClientPlayerScript>();
-            connectedListString += clientPlayerScript.playerName + "\n";
+            clientDisplayName = clientPlayerScript.playerName;
+            
+            if(clientPlayerScript.gameStarted == true){
+                if (clientPlayerScript.isTurn) clientDisplayName += " (Current Turn)";
+            }
+            else{
+                if (clientPlayerScript.ready) clientDisplayName += " (Ready)";
+            }
+            
+            clientDisplayName += "\n";
+            connectedListString += clientDisplayName;
         }
+        
         connectedPlayersText.text = connectedListString;
+        
 //        for (int i = 0; i < children; i++){
 //            ClientPlayerScript clientPlayerScript = transform.GetChild(i).gameObject.GetComponent<ClientPlayerScript>();
 //            connectedList += clientPlayerScript.playerName + "\n";
