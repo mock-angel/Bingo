28c4a8aced749ccc3666788423cbd43fb3921079
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-16 10:09:00 +0530

    Created new scene that manages connections to photon.

---
+++ Bingo/Assets/photonButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class photonButton : MonoBehaviourPunCallbacks
+{
+    public menuLogic mLogic;
+    public TMP_InputField createRoomInput, joinRoomInput;
+    // Start is called before the first frame update
+    
+    public void OnClickCreateRoom(){
+        if(createRoomInput.text.Length >= 1)
+        PhotonNetwork.CreateRoom(createRoomInput.text, new RoomOptions(){MaxPlayers = 4}, null);
+    }
+    
+    public void OnClickJoinRoom(){
+        if(joinRoomInput.text.Length >= 1)
+        PhotonNetwork.JoinRoom(joinRoomInput.text);
+    }
+    
+    public override void OnJoinedRoom(){
+        Debug.Log("We are connected to the room!");
+        mLogic.OnChangeToGameLevel();
+    }
+    
+}
--- Bingo/Assets/photonConnect.cs
+++ Bingo/Assets/photonConnect.cs
@@ -2,35 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class photonConnect : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+
+public class photonConnect : MonoBehaviourPunCallbacks
 {
-    public string versionname = "0.1";
+    public GameObject connectingView;
+    public GameObject connectedView;
+    public GameObject disconnectedView;
     
-    
-    public GameObject player;
-    
-    public GameObject connected, disconnected, connect;
-    public void connectToPhoton(){
-        PhotonNetwork.ConnectUsingSettings(versionname);
+    void Awake(){
+        PhotonNetwork.ConnectUsingSettings();
         
         Debug.Log("Connecting To photon");
     }
     
-    private void OnConnectedToMaster(){
+    public override void OnConnectedToMaster(){
         PhotonNetwork.JoinLobby(TypedLobby.Default);
         
-        connect.SetActive(false);
-        connected.SetActive(true);
-        Debug.Log("Connected to lobby");
+        Debug.Log("We are connected to master");
     }
     
-    private void OnJoinedLobby(){
+    public override void OnJoinedLobby(){
+        
+        if(connectedView != null) connectedView.SetActive(true);
+        if(connectingView != null) connectingView.SetActive(false);
         Debug.Log("On joined Lobby");
     }
     
-    private OnDisconnectedFromPhoton(){
-    
-        connected.SetActive(true);
+    public override void OnDisconnected(DisconnectCause cause){
+        if(connectingView.activeSelf)
+            connectingView.SetActive(false);
+        
+        if(connectedView.activeSelf)
+            connectedView.SetActive(false);
+        
+        disconnectedView.SetActive(true);
         Debug.Log("Disconnected from photon services");
     }
 }
